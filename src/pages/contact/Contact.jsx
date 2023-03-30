baba971a877d14f465a284c6f752ab22a86bf4f7
Serj Qazi <serjqazi@Serjs-MacBook-Pro-2.local>
2023-03-30 04:55:53 -0400

    shadow check

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Contact = () => {
   return (
-    <div className="my-4 text-slate-700">
-      <div className="mx-20 mb-2 flex items-center justify-center border-2 border-dashed border-slate-700 border-opacity-50">
-        <h1 className="py-2 text-center font-black uppercase tracking-widest text-slate-700">
+    <div className="bg-slate-100 py-2 text-slate-700 xs:py-4 landscape:min-h-[94.4vh] landscape:py-4">
+      <div className="mx-20 mb-4 flex items-center justify-center border-2 border-dashed border-slate-700 border-opacity-50 landscape:mx-36">
+        <h1 className="py-2 text-center text-xl font-black uppercase tracking-widest text-slate-700">
           Contact
         </h1>
       </div>
--- src/pages/resume/Resume.jsx
+++ src/pages/resume/Resume.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const Resume = () => {
-  return <div>Resume</div>;
+  return (
+    <div className="bg-slate-100 py-2 text-slate-700 xs:py-4 landscape:min-h-[94.4vh] landscape:py-4">
+      <div className="mx-20 mb-4 flex items-center justify-center border-2 border-dashed border-slate-700 border-opacity-50 landscape:mx-36">
+        <h1 className="py-2 text-center text-xl font-black uppercase tracking-widest text-slate-700">
+          Resume
+        </h1>
+      </div>
+    </div>
+  );
 };
 
 export default Resume;
