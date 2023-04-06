f0b8560a589796124a629e54ca6cbe423cee9941
Serj Qazi <serjqazi@Serjs-MacBook-Pro-2.local>
2023-04-06 01:39:34 -0400

    2xl laptop responsiveness about  page test

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -6,14 +6,14 @@ import webIcon from '../../assets/icons/webDesign.png';
 
 const About = () => {
   return (
-    <div className="bg-slate-100 py-2 text-slate-700 xs:py-4 lg:portrait:py-6 landscape:py-4">
-      <div className="mx-20 mb-4 flex items-center justify-center border-2 border-dashed border-slate-700 border-opacity-50 md:mx-36 lg:portrait:mx-48 lg:portrait:mb-6 landscape:mx-36 smallTab:landscape:mx-44 lg:landscape:mb-6 xl:landscape:mx-64 xl:landscape:mb-4">
-        <h1 className="py-2 text-center text-xl font-black uppercase tracking-widest text-slate-700 md:text-3xl smallTab:text-4xl xl:py-4 lg:portrait:py-4 lg:portrait:text-5xl phone:landscape:text-xl lg:landscape:text-3xl xl:landscape:py-2">
+    <div className="bg-slate-100 py-2 text-slate-700 xs:py-4 lg:portrait:py-6 landscape:py-4 2xl:landscape:py-6">
+      <div className="mx-20 mb-4 flex items-center justify-center border-2 border-dashed border-slate-700 border-opacity-50 md:mx-36 lg:portrait:mx-48 lg:portrait:mb-6 landscape:mx-36 smallTab:landscape:mx-44 lg:landscape:mb-6 xl:landscape:mx-64 xl:landscape:mb-4 2xl:landscape:mx-[24rem] 2xl:landscape:mb-6">
+        <h1 className="py-2 text-center text-xl font-black uppercase tracking-widest text-slate-700 md:text-3xl smallTab:text-4xl xl:py-4 lg:portrait:py-4 lg:portrait:text-5xl phone:landscape:text-xl lg:landscape:text-3xl xl:landscape:py-2 2xl:landscape:text-4xl">
           About Me
         </h1>
       </div>
       <div className="textContainer mx-2 md:mx-6 landscape:mx-6">
-        <div className="mb-2 items-center justify-start text-sm font-light leading-5 xs:mb-4 xs:flex xs:text-sm sm:text-base md:text-xl smallTab:text-2xl xl:text-xl lg:portrait:mb-6 lg:portrait:text-[1.7rem] landscape:mb-4">
+        <div className="mb-2 items-center justify-start text-sm font-light leading-5 xs:mb-4 xs:flex xs:text-sm sm:text-base md:text-xl smallTab:text-2xl xl:text-xl lg:portrait:mb-6 lg:portrait:text-[1.7rem] landscape:mb-4 2xl:landscape:mb-6 2xl:landscape:text-2xl">
           <h2 className=" ">
             I'm <span className="font-bold">Serj Qazi,</span>
           </h2>
@@ -25,21 +25,21 @@ const About = () => {
       </div>
 
       <div className="flex-row-reverse xl:flex ">
-        <div className="mb-4 px-2 xs:mb-5 md:px-6 xl:w-1/2 lg:portrait:mb-7 landscape:mb-4 landscape:px-6 xl:landscape:mb-0 xl:landscape:py-4">
-          <h3 className="mb-2 text-lg font-bold md:mb-4 md:text-xl smallTab:text-2xl lg:portrait:text-[1.65rem] xl:landscape:text-xl">
+        <div className="mb-4 px-2 xs:mb-5 md:px-6 xl:w-1/2 lg:portrait:mb-7 landscape:mb-4 landscape:px-6 xl:landscape:mb-0 xl:landscape:py-4 2xl:landscape:pl-16">
+          <h3 className="mb-2 text-lg font-bold md:mb-4 md:text-xl smallTab:text-2xl lg:portrait:text-[1.65rem] xl:landscape:text-xl 2xl:landscape:mb-6 2xl:landscape:text-2xl">
             What I Do?
           </h3>
 
           <div className="xl:flex-col landscape:flex">
-            <div className="mb-4 flex items-center pl-2 landscape:mb-0 landscape:pl-0 xl:landscape:mb-5">
+            <div className="mb-4 flex items-center pl-2 landscape:mb-0 landscape:pl-0 xl:landscape:mb-5 2xl:landscape:mb-7">
               <div className="logo flex w-20 items-center justify-center md:w-[5.5rem] smallTab:w-[6.5rem] lg:portrait:w-[8.5rem] phone:landscape:w-20 lg:landscape:w-[7.5rem] xl:landscape:w-[6rem] ">
                 <img src={webIcon} alt="" />
               </div>
               <div className="ml-6 w-2/3 landscape:ml-4">
-                <h3 className="mb-1 text-sm font-bold md:text-base smallTab:text-lg lg:portrait:text-[1.4rem] phone:landscape:text-sm lg:landscape:text-base">
+                <h3 className="mb-1 text-sm font-bold md:text-base smallTab:text-lg lg:portrait:text-[1.4rem] phone:landscape:text-sm lg:landscape:text-base 2xl:landscape:text-lg">
                   Development
                 </h3>
-                <p className="text-xs md:text-sm smallTab:text-base lg:portrait:text-lg phone:landscape:text-xs lg:landscape:text-sm">
+                <p className="text-xs md:text-sm smallTab:text-base lg:portrait:text-lg phone:landscape:text-xs lg:landscape:text-sm 2xl:landscape:text-base">
                   Manage / build new content for existing web pages and
                   applications using JavaScript and React.
                 </p>
@@ -50,10 +50,10 @@ const About = () => {
                 <img src={figmaIcon} alt="" />
               </div>
               <div className="ml-6 w-2/3 landscape:ml-4">
-                <h3 className="mb-1 text-sm font-bold md:text-base smallTab:text-lg lg:portrait:text-[1.4rem] phone:landscape:text-sm lg:landscape:text-base">
+                <h3 className="mb-1 text-sm font-bold md:text-base smallTab:text-lg lg:portrait:text-[1.4rem] phone:landscape:text-sm lg:landscape:text-base 2xl:landscape:text-lg">
                   Figma Design
                 </h3>
-                <p className="text-xs md:text-sm smallTab:text-base lg:portrait:text-lg phone:landscape:text-xs lg:landscape:text-sm">
+                <p className="text-xs md:text-sm smallTab:text-base lg:portrait:text-lg phone:landscape:text-xs lg:landscape:text-sm 2xl:landscape:text-base">
                   Build prototypes of web pages and applications from scratch
                   using figma and other design tools.
                 </p>
--- src/pages/about/Skills.jsx
+++ src/pages/about/Skills.jsx
@@ -18,84 +18,84 @@ import {
 const Skills = () => {
   return (
     <div className="py-2 xs:py-4 landscape:py-4">
-      <div className="mx-20 mb-4 flex items-center justify-center border-2 border-dashed border-slate-400 border-opacity-50 md:mx-32 md:mb-6 smallTab:mx-36 lg:portrait:mx-52 landscape:mb-2 phone:landscape:mb-4 lg:landscape:mx-44 lg:landscape:mb-6 xl:landscape:mx-28">
-        <h3 className="py-1 text-lg font-bold uppercase tracking-[0.35em] text-yellow-500 md:text-xl smallTab:text-xl lg:portrait:py-4 lg:portrait:text-3xl lg:landscape:py-2">
+      <div className="mx-20 mb-4 flex items-center justify-center border-2 border-dashed border-slate-400 border-opacity-50 md:mx-32 md:mb-6 smallTab:mx-36 lg:portrait:mx-52 landscape:mb-2 phone:landscape:mb-4 lg:landscape:mx-44 lg:landscape:mb-6 xl:landscape:mx-28 2xl:landscape:mx-40 2xl:landscape:mb-8">
+        <h3 className="py-1 text-lg font-bold uppercase tracking-[0.35em] text-yellow-500 md:text-xl smallTab:text-xl lg:portrait:py-4 lg:portrait:text-3xl lg:landscape:py-2 2xl:landscape:text-2xl">
           Skills
         </h3>
       </div>
 
       <div className="text-xl text-yellow-500 md:text-2xl smallTab:text-3xl lg:portrait:text-4xl">
         <ul className="grid grid-cols-4 tracking-wider landscape:grid-cols-6 xl:landscape:grid-cols-4">
-          <li className="mb-3 flex flex-col items-center justify-center lg:portrait:mb-4 landscape:text-xl">
+          <li className="mb-3 flex flex-col items-center justify-center lg:portrait:mb-4 landscape:text-xl 2xl:landscape:mb-6 2xl:landscape:text-3xl">
             <FaReact />
-            <p className="mt-2 text-center text-xs font-thin text-slate-100 md:text-sm smallTab:text-base lg:portrait:text-lg landscape:mt-3">
+            <p className="mt-2 text-center text-xs font-thin text-slate-100 md:text-sm smallTab:text-base lg:portrait:text-lg landscape:mt-3 2xl:landscape:mt-2 2xl:landscape:text-base">
               React
             </p>
           </li>
-          <li className="mb-3 flex flex-col items-center justify-center lg:portrait:mb-4 landscape:text-xl">
+          <li className="mb-3 flex flex-col items-center justify-center lg:portrait:mb-4 landscape:text-xl 2xl:landscape:mb-6 2xl:landscape:text-3xl">
             <SiJavascript />
-            <p className="mt-2 text-center text-xs font-thin text-slate-100 md:text-sm smallTab:text-base lg:portrait:text-lg landscape:mt-3">
+            <p className="mt-2 text-center text-xs font-thin text-slate-100 md:text-sm smallTab:text-base lg:portrait:text-lg landscape:mt-3 2xl:landscape:mt-2 2xl:landscape:text-base">
               JavaScript
             </p>
           </li>
-          <li className="mb-3 flex flex-col items-center justify-center lg:portrait:mb-4 landscape:text-xl">
+          <li className="mb-3 flex flex-col items-center justify-center lg:portrait:mb-4 landscape:text-xl 2xl:landscape:mb-6 2xl:landscape:text-3xl">
             <SiJquery />
-            <p className="mt-2 text-center text-xs font-thin text-slate-100 md:text-sm smallTab:text-base lg:portrait:text-lg landscape:mt-3">
+            <p className="mt-2 text-center text-xs font-thin text-slate-100 md:text-sm smallTab:text-base lg:portrait:text-lg landscape:mt-3 2xl:landscape:mt-2 2xl:landscape:text-base">
               jQuery
             </p>
           </li>
-          <li className="mb-3 flex flex-col items-center justify-center lg:portrait:mb-4 landscape:text-xl">
+          <li className="mb-3 flex flex-col items-center justify-center lg:portrait:mb-4 landscape:text-xl 2xl:landscape:mb-6 2xl:landscape:text-3xl">
             <SiHtml5 />
-            <p className="mt-2 text-center text-xs font-thin text-slate-100 md:text-sm smallTab:text-base lg:portrait:text-lg landscape:mt-3">
+            <p className="mt-2 text-center text-xs font-thin text-slate-100 md:text-sm smallTab:text-base lg:portrait:text-lg landscape:mt-3 2xl:landscape:mt-2 2xl:landscape:text-base">
               HTML 5
             </p>
           </li>
-          <li className="mb-3 flex flex-col items-center justify-center lg:portrait:mb-4 landscape:text-xl">
+          <li className="mb-3 flex flex-col items-center justify-center lg:portrait:mb-4 landscape:text-xl 2xl:landscape:mb-6 2xl:landscape:text-3xl">
             <SiCss3 />
-            <p className="mt-2 text-center text-xs font-thin text-slate-100 md:text-sm smallTab:text-base lg:portrait:text-lg landscape:mt-3">
+            <p className="mt-2 text-center text-xs font-thin text-slate-100 md:text-sm smallTab:text-base lg:portrait:text-lg landscape:mt-3 2xl:landscape:mt-2 2xl:landscape:text-base">
               CSS 3
             </p>
           </li>
-          <li className="mb-3 flex flex-col items-center justify-center lg:portrait:mb-4 landscape:text-xl">
+          <li className="mb-3 flex flex-col items-center justify-center lg:portrait:mb-4 landscape:text-xl 2xl:landscape:mb-6 2xl:landscape:text-3xl">
             <FaSass />
-            <p className="mt-2 text-center text-xs font-thin text-slate-100 md:text-sm smallTab:text-base lg:portrait:text-lg landscape:mt-3">
+            <p className="mt-2 text-center text-xs font-thin text-slate-100 md:text-sm smallTab:text-base lg:portrait:text-lg landscape:mt-3 2xl:landscape:mt-2 2xl:landscape:text-base">
               SASS
             </p>
           </li>
-          <li className="mb-3 flex flex-col items-center justify-center lg:portrait:mb-4 landscape:mb-0 landscape:text-xl">
+          <li className="mb-3 flex flex-col items-center justify-center lg:portrait:mb-4 landscape:mb-0 landscape:text-xl 2xl:landscape:mb-6 2xl:landscape:text-3xl">
             <SiTailwindcss />
-            <p className="mt-2 text-center text-xs font-thin text-slate-100 md:text-sm smallTab:text-base lg:portrait:text-lg landscape:mt-3">
+            <p className="mt-2 text-center text-xs font-thin text-slate-100 md:text-sm smallTab:text-base lg:portrait:text-lg landscape:mt-3 2xl:landscape:mt-2 2xl:landscape:text-base">
               Tailwind
             </p>
           </li>
-          <li className="mb-3 flex flex-col items-center justify-center lg:portrait:mb-4 landscape:mb-0 landscape:text-xl">
+          <li className="mb-3 flex flex-col items-center justify-center lg:portrait:mb-4 landscape:mb-0 landscape:text-xl 2xl:landscape:mb-6 2xl:landscape:text-3xl">
             <SiGithub />
-            <p className="mt-2 text-center text-xs font-thin text-slate-100 md:text-sm smallTab:text-base lg:portrait:text-lg landscape:mt-3">
+            <p className="mt-2 text-center text-xs font-thin text-slate-100 md:text-sm smallTab:text-base lg:portrait:text-lg landscape:mt-3 2xl:landscape:mt-2 2xl:landscape:text-base">
               GitHub
             </p>
           </li>
-          <li className="mb-0 flex flex-col items-center justify-center landscape:text-xl">
+          <li className="mb-0 flex flex-col items-center justify-center landscape:text-xl 2xl:landscape:text-3xl">
             <SiFirebase />
-            <p className="mt-2 text-center text-xs font-thin text-slate-100 md:text-sm smallTab:text-base lg:portrait:text-lg landscape:mt-3">
+            <p className="mt-2 text-center text-xs font-thin text-slate-100 md:text-sm smallTab:text-base lg:portrait:text-lg landscape:mt-3 2xl:landscape:mt-2 2xl:landscape:text-base">
               Firebase
             </p>
           </li>
-          <li className="mb-0 flex flex-col items-center justify-center landscape:text-xl">
+          <li className="mb-0 flex flex-col items-center justify-center landscape:text-xl 2xl:landscape:text-3xl">
             <FaDatabase />
-            <p className="mt-2 text-center text-xs font-thin text-slate-100 md:text-sm smallTab:text-base lg:portrait:text-lg landscape:mt-3">
+            <p className="mt-2 text-center text-xs font-thin text-slate-100 md:text-sm smallTab:text-base lg:portrait:text-lg landscape:mt-3 2xl:landscape:mt-2 2xl:landscape:text-base">
               RESTful API
             </p>
           </li>
 
-          <li className="mb-0 flex flex-col items-center justify-center landscape:text-xl">
+          <li className="mb-0 flex flex-col items-center justify-center landscape:text-xl 2xl:landscape:text-3xl">
             <IoMdTabletPortrait />
-            <p className="mt-2 text-center text-xs font-thin text-slate-100 md:text-sm smallTab:text-base lg:portrait:text-lg landscape:mt-3">
+            <p className="mt-2 text-center text-xs font-thin text-slate-100 md:text-sm smallTab:text-base lg:portrait:text-lg landscape:mt-3 2xl:landscape:mt-2 2xl:landscape:text-base">
               Responsive
             </p>
           </li>
-          <li className="mb-0 flex flex-col items-center justify-center landscape:text-xl">
+          <li className="mb-0 flex flex-col items-center justify-center landscape:text-xl 2xl:landscape:text-3xl">
             <FaUniversalAccess />
-            <p className="mt-2 text-center text-xs font-thin text-slate-100 md:text-sm smallTab:text-base lg:portrait:text-lg landscape:mt-3">
+            <p className="mt-2 text-center text-xs font-thin text-slate-100 md:text-sm smallTab:text-base lg:portrait:text-lg landscape:mt-3 2xl:landscape:mt-2 2xl:landscape:text-base">
               A11Y
             </p>
           </li>
